5cbad66cfc6ba89eb15f860d3704f466c5a316a4
linzy5 <linzy5@xiaopeng.com>
2022-08-08 13:54:38 +0800

    add mp4 format

--- WORKSPACE
+++ WORKSPACE
@@ -2,6 +2,7 @@ workspace(name = "OpencvBazel")
 
 new_local_repository(
     name = "opencv",
-    path = "/home/linzy5/.local/",
+    # path = "/home/linzy5/.local/",
+    path = "/home/linzy5/OpenSourceSoftware/opencv-build-4.x/install",
     build_file = "opencv.BUILD",
 )
--- WriteVideo.cpp
+++ WriteVideo.cpp
@@ -11,11 +11,13 @@ int main(int argc, char* argv[]) {
     int width = 400;
     int height = 400;
     auto mat_type = CV_8UC3;
-    auto fourcc =  cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+    // auto fourcc =  cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+    auto fourcc = cv::VideoWriter::fourcc('m', 'p', '4', 'v');
     double fps = 10;
     bool is_color = true;
 
-    std::string video_name = "/xdata/linzy5/isp_test/test_video_001.avi";
+    std::string video_name = "./test_video_001.avi"; // cv::VideoWriter::fourcc('M', 'J', 'P', 'G')
+    std::string video_name = "./test_video_001.mp4"; // cv::VideoWriter::fourcc('m', 'p', '4', 'v')
     cv::VideoWriter video_writer;
     video_writer.open(video_name, fourcc, fps, cv::Size(width, height), is_color);
     if (!video_writer.isOpened()) {
